5c2c78dbbf38c6d8b0f95a27d8a6a88d62778183
agenciabelmond <belmondgpt@gmail.com>
2026-07-23 11:06:51 -0300

    alteraçao tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,24 +44,6 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" suppressHydrationWarning>
 
-      <head>
-        {/* Google Tag Manager */}
-        <Script
-          id="google-tag-manager"
-          strategy="beforeInteractive"
-        >
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;
-            f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-NCSSNTVG');
-          `}
-        </Script>
-        {/* End Google Tag Manager */}
-      </head>
-
       <body className={`${poppins.variable} antialiased`}>
 
         {/* Google Tag Manager (noscript) */}
@@ -78,9 +60,39 @@ export default function RootLayout({
         </noscript>
         {/* End Google Tag Manager (noscript) */}
 
+
+        {/* Google Tag Manager */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+        >
+          {`
+          (function(w,d,s,l,i){
+            w[l]=w[l]||[];
+            w[l].push({
+              'gtm.start': new Date().getTime(),
+              event:'gtm.js'
+            });
+
+            var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),
+            dl=l!='dataLayer'?'&l='+l:'';
+
+            j.async=true;
+            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+
+            f.parentNode.insertBefore(j,f);
+
+          })(window,document,'script','dataLayer','GTM-NCSSNTVG');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
+
+
         <Providers>
           {children}
         </Providers>
+
 
         <Analytics />
 
